3d86f9449fca5e0835e87c82f5142d89a5a10926
ProgrammerHasan <mehedihassan8604@gmail.com>
2021-04-23 10:33:19 +0600

    flutter_webview

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Webview',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
