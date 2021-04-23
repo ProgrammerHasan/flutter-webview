fd5f58ce51cc8b50edacec9158660814c148e745
ProgrammerHasan <mehedihassan8604@gmail.com>
2021-04-23 10:35:05 +0600

    flutter_webview

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter WebView',
       theme: ThemeData(
-        primarySwatch: Colors.,
+        primarySwatch: Colors.blue,
       ),
       home: HomePage(),
     );
